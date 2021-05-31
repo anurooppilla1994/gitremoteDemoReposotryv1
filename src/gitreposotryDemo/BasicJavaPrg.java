338e303f617d89dada2e34f2a1996baa6d375aa1
anuroop <anuroop474@gmail.com>
2021-05-31 19:05:53 +0530

    First Commit..

---
+++ src/gitreposotryDemo/BasicJavaPrg.java
@@ -0,0 +1,21 @@
+package gitreposotryDemo;
+
+public class BasicJavaPrg {
+
+	public static void main(String[] args) {
+		
+		String Name = "Eswar";
+		
+		// If command
+		if (Name =="Eswar")
+		{
+			System.out.println("The name of the person is Eswar sai prasad");
+			
+		}
+		else 
+		{
+			System.out.println("The Name of the person is somthing else");
+		}
+	}
+
+}
